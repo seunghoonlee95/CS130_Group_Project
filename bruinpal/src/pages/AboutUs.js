bab4052d9d305dfcdf8ca46126105f4481186241
Danny <seunghoonlee95@gmail.com>
2022-10-30 12:29:48 -0700

    Create Navigation bar

---
+++ bruinpal/src/pages/AboutUs.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+function AboutUs() {
+  return (
+    <React.Fragment>
+      <Navbar />
+      <h1 className="comingSoon">(About us page)Coming Soon!</h1>
+    </React.Fragment>
+  );
+}
+
+export default AboutUs;
