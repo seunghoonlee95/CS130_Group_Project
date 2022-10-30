964397fac0865d036b0794dfcdc7462ade96b499
Danny <seunghoonlee95@gmail.com>
2022-10-30 12:08:07 -0700

    Set up react

---
+++ bruinpal/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import { Link } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <h1>hello world</h1>
+    </div>
+  );
+}
+
+export default App;
