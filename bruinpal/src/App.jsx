90426e8d4322f9336fd4c23f82446c51c2acb108
Danny <seunghoonlee95@gmail.com>
2022-10-31 00:10:57 -0700

    updated main page content

--- bruinpal/src/App.jsx
+++ bruinpal/src/App.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 import "./App.css";
-import { Link } from "react-router-dom";
-import Navbar from "./components/Navbar";
 import Home from './pages/Home';
-import Footer from "./components/Footer";
-import roycehall from "../src/images/roycehall.jpeg"
+
 
 function App() {
   return (
