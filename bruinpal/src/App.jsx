86eb38f4ae6becd174b74d17a3894697d03f4c48
Danny <seunghoonlee95@gmail.com>
2022-11-01 15:35:32 -0700

    Added styling but needs more styling

--- bruinpal/src/App.jsx
+++ bruinpal/src/App.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import "./App.css";
+import { Link } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Home from './pages/Home';
-
+import Footer from "./components/Footer";
+import roycehall from "../src/images/roycehall.jpeg"
 
 function App() {
   return (
