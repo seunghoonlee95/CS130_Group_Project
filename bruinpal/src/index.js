ef2890c4e53c7c7ae769250b9c635ec34236ebc8
Danny <seunghoonlee95@gmail.com>
2022-11-02 00:54:32 -0700

    Filled in main page contents

--- bruinpal/src/index.js
+++ bruinpal/src/index.js
@@ -9,6 +9,9 @@ import Home from "./pages/Home";
 import AboutUs from "./pages/AboutUs";
 import Tasks from "./pages/Tasks";
 import LoginSignup from "./pages/LoginSignup";
+import Tutoring from "./pages/Tutoring";
+import SwipeTrade from "./pages/SwipeTrade";
+import RideShare from "./pages/RideShare";
 
 const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
@@ -20,6 +23,9 @@ root.render(
       <Route path="/aboutus" element={<AboutUs />} />
       <Route path="/loginsignup" element={<LoginSignup />} />
       <Route path="/tasks" element={<Tasks />} />
+      <Route path="/tasks/tutoring" element={<Tutoring />} />
+      <Route path="/tasks/swipetrade" element={<SwipeTrade />} />
+      <Route path="/tasks/rideshare" element={<RideShare />} />
     </Routes>
   </BrowserRouter>
 );
