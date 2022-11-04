4842b3c2d2ca3d7982881b48bc379461a334ad21
dyerasi <devinyerasi@yahoo.com>
2022-11-03 17:12:36 -0700

    route category setup, see section 2 of report for which routes do will handle what calls

---
+++ backend/routes/api/user.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const router = express.Router();
+
+//migrate calls to firebase auth sdk
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
-const express = require('express'); //Line 1
-const port = process.env.PORT || 5000; //Line 3
+const express = require('express'); 
+const port = process.env.PORT || 5000; 
 const bodyParser = require('body-parser')
-const app = express(); //Line 2
+const app = express(); 
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
-// parse application/json
+// parse application/json: req.body will be json input automatically now
 app.use(bodyParser.json())
 
 // This displays message that the server running and listening to specified port
@@ -20,3 +20,12 @@ app.get('/express_backend_test', (req, res) => {
 
 //all task apis
 app.use('/api/tasks', require('./routes/api/tasks'));
+
+//all firebase related calls
+app.use('api/database', require('./routes/api/database') );
+
+//all signup, login, user auth
+app.use('api/user', require('./routes/api/user') );
+
+//all third party calls: stripe, google places, twilio, etc
+app.use('api/lib', require('./routes/api/lib') );
