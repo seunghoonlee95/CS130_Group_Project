2294f5bd8b51527898fc2522d7584b917aeba1f9
dyerasi <devinyerasi@yahoo.com>
2022-11-23 18:58:13 -0800

    update user api created. Make sure not to change username or email, only other info

--- backend/routes/api/user.js
+++ backend/routes/api/user.js
@@ -23,5 +23,16 @@ router.get('/:email', async(req, res) => {
     }
 });
 
+router.post('/update', async(req, res) => {
+    try{
+        const userData = req.body
+        const email = userData.email
+        await userService.updateUser(email, userData)
+        res.status(200).json({message: 'Success'})
+    }catch(err){
+        res.status(404).json({ error: err.message || err.toString() });
+    }
+})
+
 
 module.exports = router;
