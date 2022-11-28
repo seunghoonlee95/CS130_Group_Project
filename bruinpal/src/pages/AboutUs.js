c30f291c6b28fc7a42bb04fb14de04b3ae670114
Brian Compton <briancompton86@gmail.com>
2022-11-27 18:32:39 -0800

    Initial Update to About Us Page

--- bruinpal/src/pages/AboutUs.js
+++ bruinpal/src/pages/AboutUs.js
@@ -6,7 +6,25 @@ function AboutUs() {
   return (
     <React.Fragment>
       <Navbar />
-      <h1 className="comingSoon">(About us page)Coming Soon!</h1>
+      <div className="aboutUs">
+        <h1>About BruinPal</h1>
+        <h2>Life at UCLA can be difficult.</h2>
+        <h4>BruinPal is a service that connects Bruins with each other<br />to make everyone's lives a bit easier.<br /><br /></h4>
+        <div className="aboutUsList">
+          <div>
+            <h4>Tutoring</h4>
+            We've all needed help in a class but never knew where to get it. Offer to help others or ask for help yourself.
+          </div>
+          <div>
+            <h4>Meal Swipes</h4>
+            Use too many swipes or have some extra? Exchange them here.
+          </div>
+          <div>
+            <h4>Rides</h4>
+            It's tough to get around Los Angeles without a car. Find rideshares or make your own here.
+          </div>
+        </div>
+      </div>
       <Footer />
     </React.Fragment>
   );
