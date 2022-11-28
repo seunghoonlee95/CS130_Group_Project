8e8739cd11f4469b644ae63691aa6a707b1a638e
Hussein Hassan <h.hassan@ucla.edu>
2022-11-27 20:06:46 -0800

    Completed Email functionality

--- backend/config/sendMail.js
+++ backend/config/sendMail.js
@@ -1,15 +1,5 @@
 const sgMail = require("@sendgrid/mail");
-// require("./twilio.env");
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
-// const msg = {
-//   to: "hussein1171@g.ucla.edu",
-//   from: "bruinpal.ucla@gmail.com",
-//   subject: "test",
-//   text: "hello",
-//   html: "<strong>hello</strong>",
-// };
-// sgMail.send(msg);
 
 exports.notifyTaskTaken = async (msg_customer, msg_tasker) => {
   sgMail.send(msg_customer);
--- backend/routes/api/lib.js
+++ backend/routes/api/lib.js
@@ -3,19 +3,35 @@ const router = express.Router();
 const sendGrid = require("../../config/sendMail.js");
 
 router.post("/notifyTaskTaken", async (req, res) => {
+  let customer_html = `<p>Hi ${req.body.customername},</p>
+    <p>We found a tasker for you! Find the task details below:</p>
+    <p>Tasker name: ${req.body.tasker}<br>
+    Tasker email: ${req.body.taskeremail}<br>
+    Task Description: ${req.body.description}</p>
+    <p>Please contact ${req.body.tasker} to organize a meet-up!</p>
+    <p>BruinPal</p>`;
+  let tasker_html = `<p>Hi ${req.body.tasker},</p>
+    <p>You selected a task! Find the task details below:</p>
+    <p>Customer name: ${req.body.customername}<br>
+    Customer email: ${req.body.customeremail}<br>
+    Task Description: ${req.body.description}</p>
+    <p>Please contact ${req.body.customername} to organize a meet-up!</p>
+    <p>BruinPal</p>`;
+
   const msg_customer = {
     to: req.body.customeremail,
     from: "bruinpal.ucla@gmail.com",
     subject: "Tasker Found",
     text: "A tasker has selected your task. Please log in and make contact!",
-    html: "<strong>A tasker has selected your task. Please log in and make contact!</strong>",
+    html: customer_html,
   };
+
   const msg_tasker = {
     to: req.body.taskeremail,
     from: "bruinpal.ucla@gmail.com",
     subject: "Task Selected",
-    text: "Please contact the purschaser!",
-    html: "<strong>Please contact the purschaser</strong>",
+    text: "Please contact the purchaser!",
+    html: tasker_html,
   };
   try {
     await sendGrid.notifyTaskTaken(msg_customer, msg_tasker);
